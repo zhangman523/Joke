f6fe841a7ad508a286a3fd7a25144d10dcd70c04
zhangman <zhangman523@126.com>
2016-04-10 21:19:25 +0800

    update .gitignore

--- app/src/main/java/com/zxf/joke/ui/JokeApp.java
+++ app/src/main/java/com/zxf/joke/ui/JokeApp.java
@@ -5,6 +5,7 @@ import android.os.Environment;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.zxf.joke.data.Constant;
 import java.io.File;
+import net.youmi.android.AdManager;
 
 /**
  * Created by zhangman on 16/3/31 13:38.
@@ -14,6 +15,7 @@ public class JokeApp extends Application {
   @Override public void onCreate() {
     super.onCreate();
     Fresco.initialize(this);
+    AdManager.getInstance(this).init("", "");
     File file = new File(Constant.DATA_DIR);
     if (!file.exists()) file.mkdirs();
   }
