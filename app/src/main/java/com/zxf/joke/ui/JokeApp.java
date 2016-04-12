57d8f8f58eb7bdc1444fb8dfde3c71aa725deb02
zhangman523 <zhangman523@126.com>
2016-04-12 13:28:13 +0800

    new branch

--- app/src/main/java/com/zxf/joke/ui/JokeApp.java
+++ app/src/main/java/com/zxf/joke/ui/JokeApp.java
@@ -1,13 +1,9 @@
 package com.zxf.joke.ui;
 
 import android.app.Application;
-import android.os.Environment;
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.zxf.joke.BuildConfig;
 import com.zxf.joke.data.Constant;
 import java.io.File;
-import net.youmi.android.AdManager;
-import net.youmi.android.spot.SpotManager;
 
 /**
  * Created by zhangman on 16/3/31 13:38.
@@ -17,9 +13,6 @@ public class JokeApp extends Application {
   @Override public void onCreate() {
     super.onCreate();
     Fresco.initialize(this);
-    AdManager.getInstance(this)
-        .init(BuildConfig.YOUMI_APP_ID, BuildConfig.YOUMI_SECRET, BuildConfig.DEBUG);
-    SpotManager.getInstance(this).loadSpotAds();
     File file = new File(Constant.DATA_DIR);
     if (!file.exists()) file.mkdirs();
   }
